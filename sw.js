305687ce0fcd65e8fb441793b156d6448c12428e
EriD1225 <132091194+EriD1225@users.noreply.github.com>
2026-07-22 01:30:07 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+/* Divas Nails · service worker: deja la app disponible sin conexión */
+var CACHE='divas-nails-v1';
+var FILES=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png'];
+self.addEventListener('install',function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES);}).then(function(){return self.skipWaiting();}));
+});
+self.addEventListener('activate',function(e){
+  e.waitUntil(caches.keys().then(function(ks){
+    return Promise.all(ks.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+  }).then(function(){return self.clients.claim();}));
+});
+self.addEventListener('fetch',function(e){
+  e.respondWith(
+    caches.match(e.request,{ignoreSearch:true}).then(function(r){
+      if(r) return r;
+      return fetch(e.request).then(function(res){
+        var cp=res.clone();
+        caches.open(CACHE).then(function(c){c.put(e.request,cp);});
+        return res;
+      }).catch(function(){return caches.match('./index.html');});
+    })
+  );
+});
